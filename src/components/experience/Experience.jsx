527ccffe9b382a787aa075681a8219ca70f0cea8
maksymProkhnitskyi <maksymprokhnitskyi@gmail.com>
2023-09-07 13:30:43 -0700

    Add experience component

---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { BsFillPatchCheckFill } from 'react-icons/bs';
+import './experience.css';
+
+const Experience = () => {
+  return (
+    <section id="experience">
+      <h5>The Skills I Have</h5>
+      <h2>Skills</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Front-end Development</h3>
+          <div className="experience__content">
+          <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>React/ Next.JS</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>Angular</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>Vue.js</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>TailwindCSS</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>CSS3</h4>
+            </article>
+          </div>
+        </div>
+        <div className="experience__frontend">
+          <h3>Back-end Development</h3>
+          <div className="experience__content">
+          <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>Node.js / ExpressJS</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>Python</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>PHP</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>Laravel</h4>
+            </article>
+          </div>
+        </div>
+        <div className="experience__frontend">
+          <h3>Database</h3>
+          <div className="experience__content">
+          <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>MySQL</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>PostgreSQL</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>MongoDB</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>Firebase</h4>
+            </article>
+          </div>
+        </div>
+        <div className="experience__frontend">
+          <h3>CMS</h3>
+          <div className="experience__content">
+          <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>WordPress</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4>Shopify</h4>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Experience
