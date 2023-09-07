ec6db6f95069f8e7f4190aad1e8f79d67db2171a
maksymProkhnitskyi <maksymprokhnitskyi@gmail.com>
2023-09-07 13:30:25 -0700

    Add header component

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://github.com/maksym0815" target="_blank" rel="noreferrer" ><FaGithub /></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
